5ff9116603b78d9ac9e89d9c340e2409851c07e4
FabianForsman <fabian.forsman@gmail.com>
2025-01-31 19:50:51 +0100

    Day three completed

--- Day3/script.py
+++ Day3/script.py
@@ -1,8 +1,7 @@
 
 input = open("Day3/input.txt", "r")
 
-data = input.read()
-
+data = input.read().replace("\n", "")
 import re
 
 def part1():
@@ -16,18 +15,38 @@ def part1():
     print(tot)
 
 def part2():
-    regex = r"(mul\(\d+,\d+\))+"
-    nr = r"\d+"
-    do = r"do\(\)"
-    dont = r"don't\(\)"
-    muls = re.findall(regex, data)
-    # find all regexes that are after do() and before don't()
+    tot = 0
+    i = 0
+    do = True
+    while i < len(data):
+        if (data[i:i+4] == "do()"):
+            do = True
+            #print(data[i:i+4])
+            i += 4
+        if do and data[i] == "m":
+            if data[i+1] == "u" and data[i+2] == "l" and data[i+3] == "(":
+                begin = i
+                i +=4
+                nr1 = ""
+                while data[i] in "0123456789":
+                    nr1 += data[i]
+                    i += 1
+                nr2 = ""
+                if not data[i] == ",":
+                    continue
+                i += 1
+                while data[i] in "0123456789":
+                    nr2 += data[i]
+                    i += 1
+                #print(data[begin:i+1])
+                if data[i] == ")":
+                    tot += int(nr1) * int(nr2)
+        if data[i:i+7] == "don't()":
+            #print(data[i:i+7])
+            do = False
+            i += 7
+        i += 1
+    print(tot)
 
-    print(muls)
-    for mul in muls:
-        nrs = re.findall(nr, mul)
-
-    
-
-#part1()
+part1()
 part2()
